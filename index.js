bb1f40c44388eb6ed0ba55464fdc45375d0b5515
Gradey Cullins <gradeycullins@gmail.com>
2015-12-24 21:54:33 -0700

    styling looks like shit, game works fine tho

--- index.js
+++ index.js
@@ -1,40 +1,33 @@
-
-
-// var colors = [
-// 	"rgb(255, 0, 0)",
-// 	"rgb(0, 255, 0)",
-// 	"rgb(0, 0, 255)",
-// 	"rgb(255, 255, 0)",
-// 	"rgb(0, 255, 255)",
-// 	"rgb(255, 255, 255)"
-// ];
-
 var squares = document.querySelectorAll('.square');
 var currColor = document.querySelector('#currColor');
 var target;
 
 reset();
 
-currColor.textContent = colors[4];
-
-
-/*
-	Add event listeners
-*/
+// event listener for tiles and sum other stuff big boi
 for (var i = 0; i < squares.length; i++) {
 	squares[i].addEventListener('click', function() {
 		if (this.style.background == target) {
+			document.querySelector('#output').textContent = "Yep";
+			for (var i = 0; i < squares.length; i++) {
+				squares[i].style.visibility = 'visible';
+				squares[i].style.background = this.style.background;
+			}
+			document.querySelector('#reset').textContent = 'Play Again';
+		} else {
+			document.querySelector('#output').textContent = "Nope";
 			this.style.visibility = 'hidden';
-		};
+		}
 	});
 	squares[i].style.background = colors[i];
 }
+
 document.querySelector('#reset').addEventListener('click', function() {
 	reset();
 });
 
 function reset() {
-
+	document.querySelector('#output').innerHTML = '&nbsp';
 	currIndex = Math.floor(Math.random() * 5);
 	colors = new Array();
 	for (var i = 0; i < 5; i++) {
